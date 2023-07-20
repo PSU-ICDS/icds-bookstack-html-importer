60e898441949f300465b3fbcdebd2466716f92f7
MKHIII <139992977+MKHIII@users.noreply.github.com>
2023-07-20 10:29:06 -0400

    Update credentials.php
    Remove security credentials...don't worry that token expires soon anyways and is only a test case. Added better instructions.

--- credentials.php
+++ credentials.php
@@ -1,7 +1,8 @@
 <?php
-
+	
 $credentials = array(
-	'url' => "10.6.64.33",
-	'id' => "WFFb7V15lqf2FEVsEsMj44N1oE9zTCSP",
-	'secret' => "a7nQCadNxV0uhBp5owh5fLokujCDBImo"
+	// You need admin access to Bookstack to get your token.
+	'url' => "ip.or.domain.of.your.bookstack.instance",
+	'id' => "theIdThatReferencesYourOauthToken",
+	'secret' => "YourOauthTokenSecretPassword"
 );
