29334ea8d3f426b667da3e14d8adf7dd720d1682
MKHIII <139992977+MKHIII@users.noreply.github.com>
2023-08-02 10:26:46 -0400

    Add files via upload

--- importer.php
+++ importer.php
@@ -23,59 +23,90 @@ if (!isset($credentials)) {
 $europa = new BookStack_Client($credentials['url'], $credentials['id'], $credentials['secret'], true);
 
 // Set the Shelf and HTML import path here
-$shelf_title = "IT Shelf Import-Test1";
-$read_path = "/home/kenny/IT";
+//$shelf_title = "IT Shelf Import-Test1";
+//$read_path = "/home/kenny/IT";
 
-// Create shelf
-$shelf_id = $europa->create_shelf($shelf_title); // Replace with appropriate function
+$shelf_title = readline("Enter a shelf title: ");
+$shelf_description = readline("Enter a shelf description: ");
+$read_path = readline("Enter path to HTML (Example: /home/yourUserName/HTML): ")
+$book_id_array = [];
+
+// Create an initial shelf...will be overwritten at end
+$shelf_payload(array
+	"name" => $shelf_title,
+	"description" => $shelf_description,
+	"books" => $book_id_array[]
+	);
+$shelf = $europa->create_shelf($shelf_payload);
 
 if (isset($shelf_id) && is_integer($shelf_id)) {
-    // Get list of books from the directory structure
-    $books = $europa->get_directory_structure($read_path);
+     // Load files from directory
+     $iterator = new DirectoryIterator($read_path);
 
-    foreach ($books as $book_name => $chapters) {
-        // Create book
-        $book_id = $europa->create_book($book_name);
+    // Iterate over files
+    foreach ($iterator as $fileinfo) {
+        if (!$fileinfo->isDot()) {
+            $file_name = $fileinfo->getFilename();
 
-        if (isset($book_id) && is_integer($book_id)) {
-            foreach ($chapters as $chapter_name => $pages) {
-                // Create chapter
-                $chapter_id = $europa->create_page(array(
-                    "book_id" => $book_id,
-                    "name" => $chapter_name,
-                    "html" => "" // Set appropriate HTML content if needed
-                ));
+            // Only handle files that end with .html extension
+            if ($europa->ends_with($file_name, ".html")) {
+                $title = str_replace(array("-", "_"), " ", $file_name);
+                $title = str_replace(array(".html"), "", $title);
 
-                if (isset($chapter_id) && is_integer($chapter_id)) {
-                    foreach ($pages as $page_name) {
-                        // Check if title contains consecutive numbers at the end
-                        if (preg_match('/\d+$/', $page_name)) {
-                            echo "Title contains numbers at the end: $page_name" . PHP_EOL;
-
-                            // Remove numbers at the end of the title
-                            $page_name = preg_replace('/\d+$/', '', $page_name);
-                        }
-
-                        if (empty($page_name)) {
-                            // If string is empty, apply placeholder or extract variable
-                            // Example: $page_name = "Page Placeholder";
-                        }
-
-                        var_dump(
-                            array($page_name, $chapter_id)
-                        );
-
-                        // Build a payload to create a page
-                        $payload = array(
-                            "chapter_id" => $chapter_id,
-                            "name" => $page_name,
-                            "html" => "" // Set appropriate HTML content if needed
-                        );
-
-                        $page = $europa->create_page($payload);
+                // Check if title contains consecutive numbers at the end
+                if (preg_match('/\d+$/', $title)) {
+                    echo "Title contains numbers at the end: $title" . PHP_EOL;
+                    
+                        // Remove numbers at the end of the title
+                        $title = preg_replace('/\d+$/', '', $title);
                     }
+                } 
+                if ($title === '' || $title === ' ' || $title === '  ') {
+                    // If string is empty or has empty space(s); Apply appropriate page/chapter name
+                    echo "Title is blank after consecutive number removal...";
+                    if (is_file("{$read_path}/{$file_name}")) {
+                        $html = file_get_contents("{$read_path}/{$file_name}");
+                        $dom = new \DOMDocument();
+                        @$dom->loadHTML($html);
+                        $title_element = $dom->getElementsByTagName('title')->item(0);
+                    } else {
+                        echo "File does not exist: {$read_path}/{$file_name}";
+                    }
+                        if ($title_element) {
+                                $title = $title_element->nodeValue;
+                                echo "Auto-Title applied: $title";
                 }
+            }
+
+            // Create and name book...same as page? idk
+            $book_id = $europa->create_book($title);
+
+            if (isset($book_id) && is_integer($book_id)) {
+                //hopefully create an array of book id's to add to the shelf at the end
+                $book_id_array[] += $book_id;
+
+                var_dump(
+                    array($title, $book_id)
+                );
+
+                // Build a payload to create a page
+                $payload = array(
+                    "book_id" => $book_id,
+                    "name" => $title,
+                    "html" => file_get_contents("{$read_path}/{$file_name}"),
+                    //"tags" => array()
+                );
+
+                $page = $europa->create_page($payload);
             }
         }
     }
 }
+
+// Hopefully create the shelf down here after iterations
+$shelf_payload(array
+                    "name" => $shelf_title,
+                    "description" => $shelf_description,
+                    "books" => $book_id_array[]
+                );
+$shelf = $europa->create_shelf($shelf_payload);
